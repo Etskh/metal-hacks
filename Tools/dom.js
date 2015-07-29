5ad12630fdea3ff8915eeac43faca831da451b73
Etskh <etskh@hotmail.com>
2015-07-28 22:37:55 -0700

    Added require functionality

--- Tools/dom.js
+++ Tools/dom.js
@@ -2,6 +2,28 @@
 
 var DOMUtil = {
 
+	// Includes additional script files, and makes sure they're
+	// included on the page
+	//
+	require : function(url, callback) {
+		// from: http://stackoverflow.com/questions/950087/include-a-javascript-file-in-another-javascript-file
+		// Adding the script tag to the head as suggested before
+	    var body = document.getElementsByTagName('body')[0];
+	    var script = document.createElement('script');
+	    script.type = 'text/javascript';
+	    script.src = url;
+
+	    // Then bind the event to the callback function.
+	    // There are several events for cross browser compatibility.
+	    if( callback ) {
+			script.onreadystatechange = callback;
+	    	script.onload = callback;
+		}
+
+	    // Fire the loading
+	    body.appendChild(script);
+	},
+
 	// Use this function to add a class to a dom object
 	//
 	addClass : function( dom, className ) {
